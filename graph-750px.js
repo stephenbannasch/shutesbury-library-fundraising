9e5ac68f07ae7d50b8b6fd83f1ebd54a04515fed
Stephen Bannasch <stephen@deanbrook.org>
2011-12-01 11:49:09 -0500

    sharp ends for 750px variant

--- graph-750px.js
+++ graph-750px.js
@@ -24,22 +24,22 @@ window.onload = function () {
  r.barchart(5, 40, 195, 590, [[amount_raised], [fund_raising_goal-amount_raised]], 
     {
        stacked: true, 
-       type: "round", 
+       type: "sharp", 
        colors: colors,
     });
   r.text(100, 30, "Raising Funds").attr(txtattr1);
-  r.text(100, 110, "Goal\n" + dollar_format(fund_raising_goal)).attr(txtattr2);
+  r.text(100, 120, "Goal\n" + dollar_format(fund_raising_goal)).attr(txtattr2);
   r.text(100, 460, dollar_format(amount_raised)).attr(txtattr2);
   r.text(100, 590, "$0").attr(txtattr1);
 
   c.barchart(5, 40, 195, 590, [[amount_raised], [fund_raising_goal-amount_raised]], 
      {
         stacked: true, 
-        type: "round", 
+        type: "sharp", 
         colors: colors,
      });
    c.text(100, 30, "Lowers Taxes").attr(txtattr1);
-   c.text(100, 110, "Goal\n$" + format(goal_average_monthly_cost, 4)).attr(txtattr2);
+   c.text(100, 120, "Goal\n$" + format(goal_average_monthly_cost, 4)).attr(txtattr2);
    c.text(100, 460, "$" + format(actual_average_monthly_cost, 4)).attr(txtattr2);
    c.text(100, 590, "$" + format(orig_average_monthly_cost, 4)).attr(txtattr1);
 }
