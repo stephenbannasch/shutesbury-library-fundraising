27ebf378968b8010e219a4de42814a1d90499f6b
Stephen Bannasch <stephen@deanbrook.org>
2011-12-03 14:27:03 -0500

    more editing, layout tightening

--- graph-725px.js
+++ graph-725px.js
@@ -19,28 +19,28 @@ window.onload = function () {
      c = Raphael("cost"),
      colors1 = ['#ff0000','#00ff00','#ffffff'],
      colors2 = ['#00ff00','#ff0000','#ffffff'],
-     txtattr1 = { font: "24px 'Fontin Sans', Fontin-Sans, sans-serif", "font-weight": "bold"},
-     txtattr2 = { font: "24px 'Fontin Sans', Fontin-Sans, sans-serif", "font-weight": "bold", fill: "#fff" };
+     txtattr1 = { font: "22px 'Fontin Sans', Fontin-Sans, sans-serif", "font-weight": "bold"},
+     txtattr2 = { font: "22px 'Fontin Sans', Fontin-Sans, sans-serif", "font-weight": "bold", fill: "#fff" };
 
- r.barchart(-5, 40, 200, 590, [[amount_raised], [fund_raising_goal-amount_raised]], 
+ r.barchart(-10, 40, 190, 590, [[amount_raised], [fund_raising_goal-amount_raised]], 
     {
        stacked: true, 
        type: "sharp", 
        colors: colors1,
     });
-  r.text(95, 30, "Raising Funds").attr(txtattr1);
-  r.text(95, 120, "Goal\n" + dollar_format(fund_raising_goal)).attr(txtattr2);
-  r.text(95, 475, dollar_format(amount_raised) + "\nRaised").attr(txtattr2);
-  r.text(95, 590, "$0").attr(txtattr1);
+  r.text(85, 30, "Raising Funds").attr(txtattr1);
+  r.text(85, 120, "Goal\n" + dollar_format(fund_raising_goal)).attr(txtattr2);
+  r.text(85, 475, dollar_format(amount_raised) + "\nRaised").attr(txtattr2);
+  r.text(85, 590, "$0").attr(txtattr1);
 
-  c.barchart(-5, 50, 200, 600, [[fund_raising_goal-amount_raised], [amount_raised]], 
+  c.barchart(-10, 50, 190, 600, [[fund_raising_goal-amount_raised], [amount_raised]], 
      {
         stacked: true, 
         type: "sharp", 
         colors: colors2,
      }).scale(1, -1);
-   c.text(95, 30, "Lowers Taxes").attr(txtattr1);
-   c.text(95, 110, "From $" + format(orig_average_monthly_cost, 4) + "\nper month").attr(txtattr1);
-   c.text(95, 255, "To $" + format(actual_average_monthly_cost, 4)).attr(txtattr2);
-   c.text(95, 545, "Goal\n$" + format(goal_average_monthly_cost, 4)).attr(txtattr2);
+   c.text(85, 30, "Lowers Taxes").attr(txtattr1);
+   c.text(85, 110, "From $" + format(orig_average_monthly_cost, 4) + "\nper month").attr(txtattr1);
+   c.text(85, 255, "To $" + format(actual_average_monthly_cost, 4)).attr(txtattr2);
+   c.text(85, 545, "Goal\n$" + format(goal_average_monthly_cost, 4)).attr(txtattr2);
 }
