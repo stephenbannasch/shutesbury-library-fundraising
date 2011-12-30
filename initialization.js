e5addae1f4efb64c4104940003fabc2e7944f80a
Stephen Bannasch <stephen@deanbrook.org>
2011-12-30 17:59:47 -0500

    svg creation by raphael in chrome is larger
    ... so hack in layout fixes to position the "="

--- initialization.js
+++ initialization.js
@@ -4,7 +4,18 @@ for (var i = 0; i < amount_raised_list.length; i++) {
   amount_raised_list[i].textContent = dollar_amount_raised;
 }
 
+var raised = document.getElementById("raised");
+
 var last_updated = document.getElementById("last-updated");
 if (last_updated) { last_updated.textContent = amount_raised_date };
 
 generate_graphs();
+
+//
+var equals = document.getElementById("equals");
+var actual_graphic_width = raised.clientWidth;
+
+if (actual_graphic_width !== 240 && actual_graphic_width !== 163) {
+  equals.style.left = equals.offsetLeft + 4 + "px";
+  equals.style.top = equals.offsetTop + 4 + "px";
+}
